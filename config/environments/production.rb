d946b36b21d0b74a827c9fb6bd1a728ab85e7bb2
Andrew Klotz <andrew.klotz@hotmail.com>
2014-11-07 00:33:48 -0500

    put sendgrid dev.rb into prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.default_url_options = { :host => 'evening-bastion-2124.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.sendgrid.net",
+    :port => 587,
+    :authentication => :plain,
+    :domain => ENV['SENDGRID_DOMAIN'],
+    :user_name =>  ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+}
+
+
 end
