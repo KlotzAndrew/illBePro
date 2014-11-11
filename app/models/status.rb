ffadc8b39ea5e933a9f329069652bd7673fc75a6
Andrew Klotz <andrew.klotz@hotmail.com>
2014-11-10 22:41:04 -0500

    added more throttle functionality

--- app/models/status.rb
+++ app/models/status.rb
@@ -12,12 +12,12 @@ class Status < ActiveRecord::Base
 
   def self.update_value
     Rails.logger.info "*****Started cron at #{Time.now}*****"
-    d = Time.now
+    d = Time.now.to_i
     api_call_count = 0
     val_count = 0
-    sleep_value = 0.3
+    sleep_value = 0.8
     Ignindex.where("validation_timer > ?", 0 ).each do |x|
-      if Time.now-d > 55
+      if Time.now.to_i - d > 55
          Rails.logger.info "CRON OVERLOAD! Unable to validate #{x.summoner_name}!"
       elsif api_call_count+val_count > 60
         Rails.logger.info "API OVERLOAD! Unable to validate #{x.summoner_name}!"
@@ -34,15 +34,14 @@ class Status < ActiveRecord::Base
           url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/#{x.summoner_name}?api_key=cfbf266e-d1db-4aff-9fc2-833faa722e72"
           val_count += 1
             begin
-              summoner_data = open(url,{ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}).read
+              summoner_data = open(url,{ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE,:read_timeout=>2}).read
+              summoner_hash = JSON.parse(summoner_data)
+              x.update(summoner_id: summoner_hash["#{Ignindex.last.summoner_name.downcase}"]["id"])
             rescue Timeout::Error
-              Rails.logger.info "uri timeout request for #{x.summoner_name} on name"
-            rescue OpenURI::Error => e
+              Rails.logger.info "URI-TIMEOUT request for #{x.summoner_name} on name"
+            rescue => e
               Rails.logger.info "uri error request for #{x.summoner_name} on name"
             end
-          summoner_hash = JSON.parse(summoner_data)
-          summoner_hash["#{Ignindex.last.summoner_name.downcase}"]["id"]
-          x.update(summoner_id: summoner_hash["#{Ignindex.last.summoner_name.downcase}"]["id"])
         else
           Rails.logger.info "no update id for #{x.summoner_name}"
         end
@@ -51,15 +50,16 @@ class Status < ActiveRecord::Base
         url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/#{x.summoner_id}/masteries?api_key=cfbf266e-d1db-4aff-9fc2-833faa722e72"
         val_count += 1
           begin
-            mastery_data = open(url,{ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}).read
+            mastery_data = open(url,{ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE,:read_timeout=>2}).read
+            mastery_hash = JSON.parse(mastery_data)
+            name = mastery_hash["#{x.summoner_id}"]["pages"][0]["name"]
+            Rails.logger.info "1st page name: #{name}; should be: #{x.validation_string}"
           rescue Timeout::Error
-            Rails.logger.info "uri timeout request for #{x.summoner_name} on masteries"
-          rescue OpenURI::Error => e
+            Rails.logger.info "URI-TIMEOUT request for #{x.summoner_name} on masteries"
+          rescue => e
             Rails.logger.info "uri error request for #{x.summoner_name} on masteries"
           end
-        mastery_hash = JSON.parse(mastery_data)
-        name = mastery_hash["#{x.summoner_id}"]["pages"][0]["name"]
-        Rails.logger.info "1st page name: #{name}; should be: #{x.validation_string}"
+
 
           if name == x.validation_string
             if Ignindex.where(summoner_id: x.summoner_id).where(summoner_validated: true).count > 1
@@ -86,7 +86,7 @@ class Status < ActiveRecord::Base
     end
 
     Status.where("value > ?", 0).each do |x|
-      if Time.now-d > 55
+      if Time.now.to_i - d > 55
          Rails.logger.info "CRON OVERLOAD! Unable to validate #{x.summoner_name}!"
       elsif api_call_count+val_count > 60
         Rails.logger.info "API OVERLOAD! Unable to update challenges for #{x.summoner_name}!"
@@ -102,23 +102,24 @@ class Status < ActiveRecord::Base
             Rails.logger.info "api call for #{x.summoner_id}"
             api_call_count += 1 
               begin
-                remote4_data = open(url,{ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}).read
+                remote4_data = open(url,{ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE,:read_timeout=>2}).read
+                games_hash = JSON.parse(remote4_data)
+                valid_games = []
+                i = 0
+                games_hash["matches"].each do |match|
+                  if match["queueType"] == "RANKED_SOLO_5x5" && (match["matchCreation"] - match["matchDuration"]) >= Status.last.created_at.to_i*1000
+                    valid_games << i
+                    i = i + 1
+                  else
+                    i = i + 1
+                  end
+                end
               rescue Timeout::Error
-                 Rails.logger.info "uri timeout request for #{x.summoner_name} on stats"
-              rescue OpenURI::Error => e
+                 Rails.logger.info "URI-TIMEOUT request for #{x.summoner_name} on stats"
+              rescue => e
                 Rails.logger.info "uri error request for #{x.summoner_name} on stats"
               end
-            games_hash = JSON.parse(remote4_data)
-            valid_games = []
-            i = 0
-            games_hash["matches"].each do |match|
-              if match["queueType"] == "RANKED_SOLO_5x5" && (match["matchCreation"] - match["matchDuration"]) >= Status.last.created_at.to_i*1000
-                valid_games << i
-                i = i + 1
-              else
-                i = i + 1
-              end
-            end
+
           if x.kind == 1
             Rails.logger.info "challenge kind 1 for #{x.summoner_id}"
             if valid_games.count == 0
@@ -129,7 +130,7 @@ class Status < ActiveRecord::Base
               x.update(value: 0)
               x.update(win_value: 0)
               Rails.logger.info "updated lost first for #{x.summoner_id}"
-            elsif !games_hash["matches"][valid_games[0]]["participants"][0]["stats"]["winner"]
+            elsif games_hash["matches"][valid_games[0]]["participants"][0]["stats"]["winner"]
                x.update(game_1: {:champion_id => "#{Champion.find(games_hash["matches"][valid_games[0]]["participants"][0]["championId"]).champion}", :matchCreation => "#{games_hash["matches"][valid_games[0]]["matchCreation"]}", :win_loss => "#{games_hash["matches"][valid_games[0]]["participants"][0]["stats"]["winner"]}", :matchDuration => "#{games_hash["matches"][valid_games[0]]["matchDuration"]}", :kills => "#{games_hash["matches"][valid_games[0]]["participants"][0]["stats"]["kills"]}", :deaths => "#{games_hash["matches"][valid_games[0]]["participants"][0]["stats"]["deaths"]}", :assists => "#{games_hash["matches"][valid_games[0]]["participants"][0]["stats"]["assists"]}"})
               x.update(value: 0)
               x.update(win_value: 2)
@@ -181,8 +182,8 @@ class Status < ActiveRecord::Base
         end
       end
     end
-    Rails.logger.info "Validation calls = #{val_count} | Challenge calls = #{api_call_count} | #{((val_count + api_call_count)/(Time.now - d)).round(2)}/Second"
-    Rails.logger.info "*****Finished cron in #{Time.now - d} seconds!*****"
+    Rails.logger.info "Validation calls = #{val_count} | Challenge calls = #{api_call_count} | #{((val_count + api_call_count*1.0)/(Time.now.to_i - d)).round(2)}/Second"
+    Rails.logger.info "*****Finished cron in #{Time.now.to_i - d} seconds!*****"
     Rails.logger.info "-----------------------------------"
   end
 
