7986e6a97187e96795f83dff77a2494f6d8495c9
Andrew Klotz <andrew.klotz@hotmail.com>
2014-11-12 16:37:11 -0500

    about to do a db drop...

--- db/migrate/20141103201336_add_scores_to_status.rb
+++ db/migrate/20141103201336_add_scores_to_status.rb
@@ -2,5 +2,6 @@ class AddScoresToStatus < ActiveRecord::Migration
   def change
   	add_column :statuses, :kind, :integer
   	add_column :statuses, :points, :integer
+  	add_column :statuses, :challenge_description, :string
   end
 end
