b760414aa558a02267268d75647af7208bc844a8
Andrew Klotz <andrew.klotz@hotmail.com>
2014-11-15 15:00:18 -0500

    ordinalized score rankings

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -3,6 +3,9 @@ class ScoresController < ApplicationController
 
   def index
     @scores = Score.all
+    @users = User.all
+    @top_profiles = Score.all.where.not(user_id: nil).order(week_1: :desc).limit(5)
+    @top_summoners = Score.all.where.not(summoner_id: nil).order(week_1: :desc).limit(5)
   end
 
 
