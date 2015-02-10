e248c54902337840c78b04ff9283f06a1623ca8d
Andrew Klotz <andrew.klotz@hotmail.com>
2015-02-10 16:38:49 -0500

    about to add geodelivers

---
+++ db/migrate/20150210210250_geodeliver.rb
@@ -0,0 +1,18 @@
+class Geodeliver < ActiveRecord::Migration
+  def change
+    create_table :geotries do |t|
+    	t.integer :user_id
+    	t.string :ip_address
+    	t.float :latitude
+    	t.float :longitude
+      t.string :country_code
+      t.string :postal_code
+
+      t.string :address
+
+
+
+      t.timestamps
+    end
+  end
+end
