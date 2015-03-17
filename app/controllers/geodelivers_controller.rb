c2b6689f9a521c6d34327ee284a337a9fafc1c85
Andrew Klotz <andrew.klotz@hotmail.com>
2015-03-16 23:09:39 -0400

    de-bugging geodeliver page

--- app/controllers/geodelivers_controller.rb
+++ app/controllers/geodelivers_controller.rb
@@ -12,46 +12,47 @@ class GeodeliversController < ApplicationController
 
 	end
 
-
 	def index
 		@geodeliver = Geodeliver.find_by_user_id(current_user.id)
 
-		region = Region.find(@geodeliver.region_id)
-		@region_city = region.city
-		@region_country = region.country
-
-		@tier1_description = ""
-		@tier1_vendor = ""
+			@tier1_description = ""
+			@tier1_vendor = ""
+			
+			@tier2_description = ""
+			@tier2_vendor = ""
 		
-		if region.prize_id_tier1 == nil or region.prize_id_tier1 == "[]"
-			#catch errors so json doens't derp
-		else
-			tier1 = Prize.find(JSON.parse(region.prize_id_tier1).first)
-			@tier1_description << tier1.description
-			@tier1_vendor << tier1.vendor
+			@tier3_description = ""
+			@tier3_vendor = ""
+		
+		if @geodeliver.region_id != nil
+			region = Region.find(@geodeliver.region_id)
+			@region_city = region.city
+			@region_country = region.country
+			
+			if region.prize_id_tier1 == nil or region.prize_id_tier1 == "[]"
+				#catch errors so json doens't derp
+			else
+				tier1 = Prize.find(JSON.parse(region.prize_id_tier1).first)
+				@tier1_description << tier1.description
+				@tier1_vendor << tier1.vendor
+			end
+			
+			if region.prize_id_tier2 == nil or region.prize_id_tier2 == "[]"
+				#catch errors so json doens't derp
+			else
+				tier2 = Prize.find(JSON.parse(region.prize_id_tier2).first)
+				@tier2_description << tier2.description
+				@tier2_vendor << tier2.vendor
+			end
+			
+			if region.prize_id_tier3 == nil or region.prize_id_tier3 == "[]"
+				#catch errors so json doens't derp
+			else
+				tier3 = Prize.find(JSON.parse(region.prize_id_tier3).first)
+				@tier3_description << tier3.description
+				@tier3_vendor << tier3.vendor
+			end	
 		end
-
-		@tier2_description = ""
-		@tier2_vendor = ""
-		
-		if region.prize_id_tier2 == nil or region.prize_id_tier2 == "[]"
-			#catch errors so json doens't derp
-		else
-			tier2 = Prize.find(JSON.parse(region.prize_id_tier2).first)
-			@tier2_description << tier2.description
-			@tier2_vendor << tier2.vendor
-		end
-
-		@tier3_description = ""
-		@tier3_vendor = ""
-		
-		if region.prize_id_tier3 == nil or region.prize_id_tier3 == "[]"
-			#catch errors so json doens't derp
-		else
-			tier3 = Prize.find(JSON.parse(region.prize_id_tier3).first)
-			@tier3_description << tier3.description
-			@tier3_vendor << tier3.vendor
-		end		
 
 
 	end	
