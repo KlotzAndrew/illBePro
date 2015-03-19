043dfec6fa9ebc9a5a6d5f88b119e9f323a58ac9
Andrew Klotz <andrew.klotz@hotmail.com>
2015-03-19 18:29:58 -0400

    changed homescreen text

--- app/models/status.rb
+++ app/models/status.rb
@@ -394,9 +394,12 @@ end
 
 
 def update_champions
-p = 420
-while p < 422
-Champion.create(:id => p)
+p = 1
+puts p
+while p < 500
+  if Champion.where(id: p).empty?
+    Champion.create(:id => p)
+  end
 begin
 url = "https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/#{p}?api_key=cfbf266e-d1db-4aff-9fc2-833faa722e72"
 remote5_data = open(url,{ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}).read
