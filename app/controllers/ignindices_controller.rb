789d7610ad0d5a460953f8328d5ac1e87a2abcb6
Andrew Klotz <andrew.klotz@hotmail.com>
2015-06-21 23:48:51 -0400

    fixed bug creating nil ign objects

--- app/controllers/ignindices_controller.rb
+++ app/controllers/ignindices_controller.rb
@@ -358,9 +358,8 @@ class IgnindicesController < ApplicationController
         format.json { head :no_content } 
       end         
     elsif params["commit"] == "Add Postal/Zip Code"
-      @ignindex.update(
-        :postal_code => ignindex_params[:postal_code])
-      @ignindex.update_region_id(ignindex_params[:postal_code])
+      # @ignindex.postal_code = ignindex_params[:postal_code])
+      update_region_id(@ignindex, ignindex_params[:postal_code])
 
       if user_signed_in?
         if current_user.id == @ignindex.user_id && @ignindex.summoner_validated == true
@@ -434,9 +433,9 @@ class IgnindicesController < ApplicationController
       Rails.logger.info "params_psotal_code: #{ignindex_params[:postal_code]}"
       @ignindex = Ignindex.new(
         :postal_code => ignindex_params[:postal_code])
-      @ignindex.postal_code = session[:postal_code]
+
       Rails.logger.info "self.postal_code: #{@ignindex.postal_code}"
-      @ignindex.update_region_id(ignindex_params[:postal_code]) #gets region_id from postal code
+      update_region_id(@ignindex, ignindex_params[:postal_code]) #gets region_id from postal code
       
       session[:region_id_temp] = @ignindex.region_id_temp
       session[:postal_code_temp] = ignindex_params[:postal_code]
@@ -466,7 +465,7 @@ class IgnindicesController < ApplicationController
         format.html { redirect_to new_status_path }
         format.json { head :no_content } 
       end        
-    else # save action for ign
+    elsif params[:commit] == "Add Summoner Name" # save action for ign
       if params["ignindex"]["summoner_name"].length < 1
         Rails.logger.info "name entered was nil"
         Rails.logger.info "params.legth: #{params["ignindex"]["summoner_name"].length}"
@@ -508,8 +507,49 @@ class IgnindicesController < ApplicationController
         redirect_to summoner_path
       end
 
+    else
+      #error?
     end
 
+  end
+
+  def update_region_id(ignindex, dirty_postal) #line 17 & 26 are creating when not required
+    @ignindex = ignindex
+    Rails.logger.info "method postal: #{ignindex.postal_code}, dity_postal: #{dirty_postal}"
+    postal_search = dirty_postal.to_s
+
+    #auto-detect where the postal code is from + format it for search
+    Rails.logger.info "us?: #{!/[0-9]/.match(postal_search[0]).nil?}"
+    Rails.logger.info "ca?: #{!/[a-zA-Z]/.match(postal_search[0]).nil?}"
+    if !/[0-9]/.match(postal_search[0]).nil? #this is a zip code
+      if postal_search.length > 5
+        postal_search = postal_search[0..4]
+      end
+      if !Region.where("postal_code = ?", postal_search).first.nil?
+        @ignindex.region_id_temp = Region.where("postal_code = ?", postal_search).first.id
+        # self.update(region_id_temp: Region.where("postal_code = ?", postal_search).first.id)
+      end
+      Rails.logger.info "US_postal: #{postal_search}"
+      Rails.logger.info "US_region: #{ignindex.region_id_temp}"
+    elsif !/[a-zA-Z]/.match(postal_search[0]).nil? #this is a postal code
+      if postal_search.length >= 3
+        postal_search = postal_search[0..2].upcase
+      end 
+      if !Region.where("postal_code = ?", postal_search).first.nil?
+        @ignindex.region_id_temp = Region.where("postal_code = ?", postal_search).first.id
+        # self.update(region_id_temp: Region.where("postal_code = ?", postal_search).first.id)
+      end
+      Rails.logger.info "CA_postal: #{postal_search}"
+      Rails.logger.info "CA_region: #{ignindex.region_id_temp}"
+    else
+
+      Rails.logger.info "alkatraz"
+      #error entering postal code!
+    end
+    Rails.logger.info "#postal_search: #{postal_search}"
+    # Rails.logger.info "region_id: #{Region.where("postal_code = ?", postal_search).first.id}"
+    # self.update(
+    #   :region_id => Region.where("postal_code = ?", self.postal_code).first.id)
   end
 
   private
--- app/models/ignindex.rb
+++ app/models/ignindex.rb
@@ -5,41 +5,41 @@ class Ignindex < ActiveRecord::Base
 	belongs_to :region
 	has_many :achievements
 
-	def update_region_id(dirty_postal) #line 17 & 26 are creating when not required
-		Rails.logger.info "method postal: #{self.postal_code}, dity_postal: #{dirty_postal}"
-		postal_search = dirty_postal.to_s
+	# def update_region_id(dirty_postal) #line 17 & 26 are creating when not required
+	# 	Rails.logger.info "method postal: #{self.postal_code}, dity_postal: #{dirty_postal}"
+	# 	postal_search = dirty_postal.to_s
 
-		#auto-detect where the postal code is from + format it for search
-		Rails.logger.info "us?: #{!/[0-9]/.match(postal_search[0]).nil?}"
-		Rails.logger.info "ca?: #{!/[a-zA-Z]/.match(postal_search[0]).nil?}"
-		if !/[0-9]/.match(postal_search[0]).nil? #this is a zip code
-			if postal_search.length > 5
-				postal_search = postal_search[0..4]
-			end
-			if !Region.where("postal_code = ?", postal_search).first.nil?
-				self.update(region_id_temp: Region.where("postal_code = ?", postal_search).first.id)
-			end
-			Rails.logger.info "US: #{postal_search}"
-			Rails.logger.info "US: #{self.region_id_temp}"
-		elsif !/[a-zA-Z]/.match(postal_search[0]).nil? #this is a postal code
-			if postal_search.length >= 3
-				postal_search = postal_search[0..2].upcase
-			end	
-			if !Region.where("postal_code = ?", postal_search).first.nil?
-				self.update(region_id_temp: Region.where("postal_code = ?", postal_search).first.id)
-			end
-			Rails.logger.info "CA: #{postal_search}"
-			Rails.logger.info "CA: #{self.region_id_temp}"
-		else
+	# 	#auto-detect where the postal code is from + format it for search
+	# 	Rails.logger.info "us?: #{!/[0-9]/.match(postal_search[0]).nil?}"
+	# 	Rails.logger.info "ca?: #{!/[a-zA-Z]/.match(postal_search[0]).nil?}"
+	# 	if !/[0-9]/.match(postal_search[0]).nil? #this is a zip code
+	# 		if postal_search.length > 5
+	# 			postal_search = postal_search[0..4]
+	# 		end
+	# 		if !Region.where("postal_code = ?", postal_search).first.nil?
+	# 			self.update(region_id_temp: Region.where("postal_code = ?", postal_search).first.id)
+	# 		end
+	# 		Rails.logger.info "US: #{postal_search}"
+	# 		Rails.logger.info "US: #{self.region_id_temp}"
+	# 	elsif !/[a-zA-Z]/.match(postal_search[0]).nil? #this is a postal code
+	# 		if postal_search.length >= 3
+	# 			postal_search = postal_search[0..2].upcase
+	# 		end	
+	# 		if !Region.where("postal_code = ?", postal_search).first.nil?
+	# 			self.update(region_id_temp: Region.where("postal_code = ?", postal_search).first.id)
+	# 		end
+	# 		Rails.logger.info "CA: #{postal_search}"
+	# 		Rails.logger.info "CA: #{self.region_id_temp}"
+	# 	else
 
-			Rails.logger.info "alkatraz"
-			#error entering postal code!
-		end
-		Rails.logger.info "#postal_search: #{postal_search}"
-		# Rails.logger.info "region_id: #{Region.where("postal_code = ?", postal_search).first.id}"
-		# self.update(
-		# 	:region_id => Region.where("postal_code = ?", self.postal_code).first.id)
-	end
+	# 		Rails.logger.info "alkatraz"
+	# 		#error entering postal code!
+	# 	end
+	# 	Rails.logger.info "#postal_search: #{postal_search}"
+	# 	# Rails.logger.info "region_id: #{Region.where("postal_code = ?", postal_search).first.id}"
+	# 	# self.update(
+	# 	# 	:region_id => Region.where("postal_code = ?", self.postal_code).first.id)
+	# end
 
 	def refresh_summoner
 		# self.update(summoner_validated: false)
