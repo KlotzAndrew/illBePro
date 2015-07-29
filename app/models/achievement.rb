2cbb053740a7d0e1a7ea5ad1b5b0eefd89f005c4
Andrew Klotz <andrew.klotz@hotmail.com>
2015-07-29 15:54:14 -0400

    keys needed to be hidden

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -5,13 +5,4 @@ class Achievement < ActiveRecord::Base
 
 	validates :challenge_id, presence: true
 
-	#achievement.kind 1 == "Cora Pizza"
-	# cora_postals = ["M6G", "M6J", "M5R", "M5S", "M5T", "M5G"]
-	# cora_regions = [43871, 43873, 43859, 43860, 43861, 43851]
-
-	# cora_regions.each do |x|
-	# Region.find(x).update(
-	# :prize_id_tier1 => "[1]")
-	# end
-
 end
