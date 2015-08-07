5d46d4a5b157ca13243bb9f2a03d35559fd8c766
Andrew Klotz <andrew.klotz@hotmail.com>
2015-08-07 14:29:29 -0400

    needed status controler refactor

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,7 +1,6 @@
  class StatusesController < ApplicationController
-  before_action :set_status, only: [:show]
+  before_action :set_status, only: [:show, :update, :destroy]
   before_filter :authenticate_user!
-
 
   respond_to :html, :xml, :json
 
@@ -51,65 +50,20 @@
   end
 
   def update
-    
-    if @status.roll_status == 1 #UPDATE (using AJAX for calls, redirect making no sense)
-      @status.update(roll_status: 1)
-         respond_to do |format|
-            format.html { redirect_to root_path, notice: 'Challenge getting started' }
-            format.json { head :no_content }
-            format.js { render :nothing => true}
-          end      
-    else
-
-      @status.update(trigger_timer: Time.now.to_i)
-      respond_to do |format|
-        format.html { redirect_to root_path, notice: 'Checking game results...' }
-        format.json { head :no_content }
-        format.js { render :nothing => true } 
-      end
-
+    @status.update(trigger_timer: Time.now.to_i)
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: 'Checking game results...' }
     end
   end
 
   def destroy
-    if @status.kind == 6
-       Rails.logger.info "destroy controller triggered, kind 6"
-      @status.update(
-        :value => 0,
-        :win_value => 0)
-      Prize.where(assignment: 1).where(user_id: current_user.id).last.update( {
-        :assignment => 0,
-        :user_id => 0,
-        })
-      respond_to do |format|
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
-    elsif @status.kind == 5
-      Rails.logger.info "destroy controller triggered, kind 5"
-      @status.update(value: 0)
-      @status.update(win_value: 3)
-      # score = Score.find_by_user_id(current_user.id)
-      # if score.challenge_points > 0
-      #   score.update(challenge_points: score.challenge_points - 1) 
-      # end
-      respond_to do |format|
-        format.html { redirect_to root_path }
-        format.json { head :no_content }
-      end
-    else
-      Rails.logger.info "destroy controller triggered, type nil"
-      @status.update(value: 0)
-      @status.update(win_value: 3)      
-      respond_to do |format|
-        format.html { redirect_to root_path, alert: 'Something went wrong with your challenge kind' }
-        format.json { head :no_content }
-      end      
-    end
+    @status.update(
+      :value => 0,
+      :win_value => 3)
+    redirect_to root_path
   end
 
   private
-
 
     def set_profile_ignindex
       @ignindex = current_user.ignindex
