396df1259c7ec42e6472818814f522a4b5438f04
Andrew Klotz <andrew.klotz@hotmail.com>
2015-08-10 18:09:27 -0400

    prizing needed to be a method

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,6 +1,7 @@
 class Achievement < ActiveRecord::Base
 	belongs_to :ignindex
 	belongs_to :challenge
+	belongs_to :region
 	has_many :statuses
 
 	validates :challenge_id, presence: true
--- app/models/prize.rb
+++ app/models/prize.rb
@@ -1,5 +1,4 @@
 class Prize < ActiveRecord::Base
-
 	has_many :prize_regions
 	has_many :regions, through: :prize_regions
 	has_one :ignindex
@@ -15,4 +14,32 @@ class Prize < ActiveRecord::Base
 		end
 	end
 
+	def self.assign_weekly_prizes(start_time)
+		Achievement.all.includes(:challenge).where("created_at > ?", start_time).where(has_prizing: true).where(result: 2).each do |achievement|
+			check_for_prizing(achievement)
+		end
+	end
+
+	def self.check_for_prizing(achievement)
+		if achievement.challenge.local_prizing == true
+			prize = achievement.region.prizes.where(assignment: 0).sample(1).first
+			give_user_prize(prize)
+		elsif achievement.challenge.global_prizing == true
+			prize = Prize.all.where(country_zone: achievement.challenge.country).where(vendor: achievement.merchant).where(assignment: 0).sample(1).first
+			give_user_prize(prize)
+		end
+	end
+
+	def self.give_user_prize(prize)
+		if !prize.nil?
+			ActiveRecord::Base.transaction do
+				prize.update(
+					:assignment => 1,
+					:delivered_at => Time.now.to_i,
+					:ignindex_id => achievement.ignindex_id)
+				achievement.ignindex.update(
+					:prize_id => prize_id)
+			end
+		end
+	end
 end
