bd2b0a9a295a309b3d4a2f991731e479e24054d5
Andrew Klotz <andrew.klotz@hotmail.com>
2015-08-10 17:30:08 -0400

    needed champion update method

--- app/models/champion.rb
+++ app/models/champion.rb
@@ -1,24 +1,26 @@
 class Champion < ActiveRecord::Base
 
-def update_champions
-p = 220
-puts p
-while p < 224
-  if Champion.where(id: p).empty?
-    Champion.create(:id => p)
-  end
-begin
-url = "https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/#{p}?api_key=cfbf266e-d1db-4aff-9fc2-833faa722e72"
-remote5_data = open(url,{ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}).read
-champion_hash = JSON.parse(remote5_data)
-Champion.find(p).update(champion: "#{champion_hash["key"]}")
-puts champion_hash["key"]
-p += 1
-rescue OpenURI::HTTPError => ex
-puts "KANYE for #{p}"
-p += 1
-end
-end
-end
+	def self.update_champions
+		p = 1
+		puts p
+		while p < 500
+			if Champion.where(id: p).empty?
+				Champion.create(:id => p)
+			end
+			champion = Champion.find(p)
+			if champion.champion.nil?
+				begin
+					url = "https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/#{p}?api_key=" + + Rails.application.secrets.league_api_key
+					remote5_data = open(url,{ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}).read
+					champion_hash = JSON.parse(remote5_data)
+					Champion.find(p).update(champion: "#{champion_hash["key"]}")
+					puts champion_hash["key"]
+				rescue OpenURI::HTTPError => ex
+					puts "KANYE for #{p}"
+				end
+			end
+			p += 1
+		end
+	end
 
 end
